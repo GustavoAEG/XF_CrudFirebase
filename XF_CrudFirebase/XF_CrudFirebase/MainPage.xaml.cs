a96af8b5375b151e26652c8df147ea4a6af58ebf
Gustavo <gustavoenriccoga@gmail.com>
2022-11-15 18:31:41 -0300

    001 - metodo Exibir

--- XF_CrudFirebase/XF_CrudFirebase/MainPage.xaml.cs
+++ XF_CrudFirebase/XF_CrudFirebase/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using XF_CrudFirebase.Services;
 
@@ -44,10 +45,27 @@ namespace XF_CrudFirebase
                 await DisplayAlert("Success", "Não existe contato com esse ID", "OK");
             }
         }
-        private void BtnDeletar_Clicked(object sender, System.EventArgs e)
-        { }
-        private void BtnAtualizar_Clicked(object sender, System.EventArgs e)
-        { }
-    
-}
+        private async void BtnDeletar_Clicked(object sender, System.EventArgs e)
+        {
+            await fbService.DeleteContato(Convert.ToInt32(txtId.Text));
+            txtId.Text = string.Empty;
+            await DisplayAlert("Success", "Contato deletado com sucesso", "OK");
+            await ExibeContatos();
+        }
+        private async void BtnAtualizar_Clicked(object sender, System.EventArgs e)
+        {
+            await fbService.UpdateContato(Convert.ToInt32(txtId.Text), txtNome.Text, txtEmail.Text);
+            txtId.Text = string.Empty;
+            txtNome.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            await DisplayAlert("Success", "Contato atualizado com sucesso", "OK");
+            await ExibeContatos();
+        }
+        private async Task ExibeContatos()
+        {
+            var contatos = await fbService.GetContatos();
+            listaContatos.ItemsSource = contatos;
+        }
+    }
+
 }
